d910aed3e2764868bc5ad5509b8f22d348e15afa
amichelo <andrea.michelotti@lnf.infn.it>
2022-10-12 11:15:53 +0200

    example monitoring the image array

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # py_ad_camera
 Python area detector viewer
+
+camera example docs:
+https://confluence.infn.it/display/LDCG/BASLER+CAMERA
+
+### requirements
+https://zzun.app/repo/epics-base-pvaPy-python-miscellaneous
+
+### environment
+Remeber to set:
+export EPICS_PVA_ADDR_LIST=193.206.86.152
+export EPICS_CA_AUTO_ADDR_LIST=NO
+
--- py_ad_camera.py
+++ py_ad_camera.py
@@ -1,2 +1,19 @@
 import pvaccess
+import time
 print("ciao")
+
+chan = pvaccess.Channel("13ARV1:Pva1:Image")
+
+## function called each image update
+def imageUpdate(x):
+    ## replace with the code to display monochrome image
+    print ("got array " +str(x) )
+
+
+chan.subscribe('image',imageUpdate)
+chan.setMonitorMaxQueueLength(3)
+
+chan.startMonitor()
+
+while True:
+    time.sleep (1)
