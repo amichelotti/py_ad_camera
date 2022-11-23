bc1870faf34817503026b34a935de940e3c6ea7e
amichelo <andrea.michelotti@lnf.infn.it>
2022-11-23 16:28:36 +0100

    dont use pva

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ export EPICS_PVA_AUTO_ADDR_LIST=NO
 ## install & configure a conda environment
 conda create -n "myenvironment" opencv
 conda activate myenvironment
-conda install pvapy
+conda install -c epics pvapy
 conda env config vars set EPICS_PVA_ADDR_LIST=193.206.86.152
 conda env config vars set EPICS_PVA_AUTO_ADDR_LIST=NO
 
--- py_ad_camera.py
+++ py_ad_camera.py
@@ -1,11 +1,14 @@
 import pvaccess
 import time
 import cv2
+import sys
 import numpy as np
 
-name="13ARV1"
-
-chan = pvaccess.Channel(name+":Pva1:Image")
+name="TEST"
+if len(sys.argv) > 1:
+    name=sys.argv[1]
+    
+chan = pvaccess.Channel(name+":image1:ArrayData")
 sizex_chan= pvaccess.Channel(name+":cam1:SizeX")
 sizey_chan= pvaccess.Channel(name+":cam1:SizeY")
 
@@ -13,23 +16,25 @@ sizex=sizex_chan.get()['value']
 sizey=sizey_chan.get()['value']
 
 print ("Image :"+str(sizex)+" x "+str(sizey))
-
+def pippoletto(x):
+    print(x)
+    
 ## function called each image update
 def imageUpdate(x):
    # print ("got array " +str(x['value']['ubyteValue']) )
     arr=pvaccess.NtNdArray(x)
-    ts = arr.getDataTimeStamp()
+    # ts = arr.getDataTimeStamp()
     
-    print ("TS:"+str(ts))
+    # print ("TS:"+str(arr))
 
-    im = arr.getValue()['ubyteValue'].reshape((sizey, sizex)) #notice row, column format
+    im = arr.getValue()['ubyte'].reshape((sizey, sizex)) #notice row, column format
     ## replace with the code to display monochrome image
     cv2.imshow(name, im)
     cv2.waitKey(1)
 
 chan.subscribe('image',imageUpdate)
 chan.setMonitorMaxQueueLength(3)
-
+pippoletto("ciao")
 chan.startMonitor()
 
 while True:
